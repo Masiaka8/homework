e9138273e46a4af913d9f45bdd72119e11621f62
Masiaka8 <masiaka8@outlook.com>
2022-10-13 22:11:56 +0300

    Commit 2

--- src/test2/task22.java
+++ src/test2/task22.java
@@ -2,6 +2,23 @@ package test2;
 
 public class task22 {
     public static void main(String[] args) {
-
+        int a = 0;
+        for (int i = 1; i <= 99; i++) {
+            if (i % 2 != 0)
+                a++;
+        }
+        int[] mas = new int[a];
+        int index = 0;
+        for (int i = 1; i <= 99; i++) {
+            if (i % 2 != 0) {
+                mas[index] = i;
+                System.out.print(mas[index] + " ");
+                index++;
+            }
+        }
+        System.out.println(" ");
+        for (int i = mas.length - 1; i >= 0; i--) {
+            System.out.println(mas[i]);
+        }
     }
 }
